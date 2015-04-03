e2f5593fb029b7dbafe8a7e089668e73767c2c1d
Taylor Andrews <andrewstaylorj@gmail.com>
2015-04-03 12:06:37 -0600

    Completed multi-lookup

--- CSCI-3753/PA3/multi-lookup.c
+++ CSCI-3753/PA3/multi-lookup.c
@@ -13,65 +13,235 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
+#include <unistd.h> 
 
 #include "multi-lookup.h"
 #include "util.h"
+#include "queue.h"
+
+queue buf;
+
+pthread_mutex_t buf_lock;
+pthread_mutex_t file_lock;
+pthread_mutex_t requester_lock;
+
+int current_requesters;
 
 int main(int argc, char* argv[]){
 
-    /* Local Vars */
-    FILE* inputfp = NULL;
-    FILE* outputfp = NULL;
-    char hostname[SBUFSIZE];
-    char errorstr[SBUFSIZE];
-    char firstipstr[INET6_ADDRSTRLEN];
-    int i;
-    
     /* Check Arguments */
-    if(argc < MINARGS){
+    if (argc < MINARGS) {
 	fprintf(stderr, "Not enough arguments: %d\n", (argc - 1));
 	fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
 	return EXIT_FAILURE;
     }
 
+    /* Local Vars */
+    FILE* outputfp = NULL;
+
+    int i;
+    int rc; 
+    int num_requesters = argc - 2; //Total number of inputs minus 2 (for executable and output)
+    int num_resolvers = sysconf(_SC_NPROCESSORS_ONLN);
+    
+    current_requesters = num_requesters;
+
+    /* Check Number of Resolvers */
+    if (num_resolvers < MIN_RESOLVER_THREADS) {
+        num_resolvers = MIN_RESOLVER_THREADS;
+	}
+
+    /* Create Arrays for pthreads */
+    pthread_t requesters[num_requesters];
+    pthread_t resolvers[num_resolvers];
+
     /* Open Output File */
     outputfp = fopen(argv[(argc-1)], "w");
-    if(!outputfp){
-	perror("Error Opening Output File");
+    if (!outputfp) {
+        fprintf(stderr, "Error opening output file \n");
 	return EXIT_FAILURE;
     }
 
-    /* Loop Through Input Files */
-    for(i=1; i<(argc-1); i++){
-	
-	/* Open Input File */
-	inputfp = fopen(argv[i], "r");
-	if(!inputfp){
-	    sprintf(errorstr, "Error Opening Input File: %s", argv[i]);
-	    perror(errorstr);
-	    break;
-	}	
+    /* Create the Queue */
+    if (queue_init(&buf, BUF_SIZE) == QUEUE_FAILURE) {
+        fprintf(stderr, "Initializing queue failed \n");
+	return EXIT_FAILURE;
+    }
 
-	/* Read File and Process*/
-	while(fscanf(inputfp, INPUTFS, hostname) > 0){
-	
-	    /* Lookup hostname and get IP string */
-	    if(dnslookup(hostname, firstipstr, sizeof(firstipstr))
-	       == UTIL_FAILURE){
-		fprintf(stderr, "dnslookup error: %s\n", hostname);
-		strncpy(firstipstr, "", sizeof(firstipstr));
-	    }
-	
-	    /* Write to Output File */
-	    fprintf(outputfp, "%s,%s\n", hostname, firstipstr);
+    /* Create Mutex */
+    if (pthread_mutex_init(&buf_lock, NULL)) {
+        fprintf(stderr, "Creating buf mutex failed \n");
+	return EXIT_FAILURE;
+    }
+
+    if (pthread_mutex_init(&file_lock, NULL)) {
+        fprintf(stderr, "Creating file mutex failed \n");
+	return EXIT_FAILURE;
+    }
+
+    if (pthread_mutex_init(&requester_lock, NULL)) {
+        fprintf(stderr, "Creating requester mutex failed \n");
+	return EXIT_FAILURE;
+    }
+
+    /* Spawn Requeters */
+    for (i = 0; i < num_requesters; i++) {
+      rc = pthread_create(&requesters[i], NULL, requester, argv[i+1]);
+	if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+	    exit(EXIT_FAILURE);
 	}
+    }
 
-	/* Close Input File */
-	fclose(inputfp);
+    /* Spawn Resolvers */
+    for (i = 0; i < num_resolvers; i++) {
+      rc = pthread_create(&resolvers[i], NULL, resolver, (void*)outputfp);
+	if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+	    exit(EXIT_FAILURE);
+	}
+    }
+
+    /* Wait for Threads */
+    for (i = 0; i < num_requesters; i++) {
+        pthread_join(requesters[i], NULL);
+    }
+    
+    for (i = 0; i < num_resolvers; i++) {
+        pthread_join(resolvers[i], NULL);
     }
 
     /* Close Output File */
-    fclose(outputfp);
+    if (fclose(outputfp)) {
+        fprintf(stderr, "Error closing output file \n");
+    }
+
+    /* Cleanup Queue */
+    queue_cleanup(&buf);
+
+    /* Cleanup Mutex */
+    if (pthread_mutex_destroy(&buf_lock)) {
+        fprintf(stderr, "Destroying buf mutex failed \n");
+    } 
+
+    if (pthread_mutex_destroy(&file_lock)) {
+        fprintf(stderr, "Destroying file mutex failed \n");
+    }
+    
+    if (pthread_mutex_destroy(&requester_lock)) {
+        fprintf(stderr, "Destroying requester mutex failed \n");
+    }
 
     return EXIT_SUCCESS;
+}
+
+void* requester(void* input) {
+    FILE* inputfp;
+
+    char* payload;
+    char hostname[SBUFSIZE];
+
+    /* Open File */
+    inputfp = fopen((char*)input, "r");
+
+    if (!inputfp) {
+        fprintf(stderr, "Error opening input file \n");
+
+	pthread_mutex_lock(&requester_lock);
+	current_requesters--;
+	pthread_mutex_unlock(&requester_lock);
+
+	return NULL;
+    }   
+
+    while (fscanf(inputfp, INPUTFS, hostname) > 0) {
+        pthread_mutex_lock(&buf_lock);
+	while (queue_is_full(&buf)) {
+  	    pthread_mutex_unlock(&buf_lock);
+	    if(usleep(rand() % 100)) {
+	        fprintf(stderr, "Error sleeping thread \n");
+	    }
+	    pthread_mutex_lock(&buf_lock);
+	}
+	
+	if (!queue_is_full(&buf)) {
+	    payload = malloc(sizeof(hostname));
+	    strcpy(payload, hostname);
+	    
+	    if (queue_push(&buf, (void*) payload) == QUEUE_FAILURE) {
+	        fprintf(stderr, "Queue push failed \n");
+	    }
+	}
+	else {
+	    fprintf(stderr, "Error accessing thread \n");
+	}
+
+	pthread_mutex_unlock(&buf_lock);
+    }
+
+    /* Close file */;
+    if (fclose(inputfp)) {
+        fprintf(stderr, "Error closing input file \n");
+    }
+
+    pthread_mutex_lock(&requester_lock);
+    current_requesters--;
+    pthread_mutex_unlock(&requester_lock);
+
+    return NULL;
+}
+
+void* resolver(void* output) {
+    FILE* outputfp = output;
+
+    char* hostname;
+
+    node_t* headptr;
+    node_t* temp;
+
+    pthread_mutex_lock(&buf_lock);
+    while (current_requesters || !queue_is_empty(&buf)) {
+        if ((hostname = (char*)queue_pop(&buf)) == NULL) {
+	    fprintf(stderr, "Queue pop failed \n");
+        }
+	pthread_mutex_unlock(&buf_lock);
+
+	headptr = malloc(sizeof(node_t));
+	headptr->next = NULL;
+
+	if (extended_dnslookup(hostname, headptr, INET6_ADDRSTRLEN) == UTIL_FAILURE) {
+		fprintf(stderr, "dnslookup error: %s\n", hostname);
+	}
+
+	pthread_mutex_lock(&file_lock);
+	fprintf(outputfp, "%s", hostname);
+
+	/* Handle Printing Edge Cases */
+	if (headptr->next == NULL) {
+	    fprintf(outputfp, ",");
+	}
+	else {
+	    int j = 0;
+	    while (headptr->next != NULL) {
+	        if (j % 3 == 0)
+	            fprintf(outputfp, ",%s", headptr->data);
+		j++;
+		temp = headptr; 
+		headptr = headptr->next;
+		free(temp); //Free the allocated memory
+	    }
+	}
+	fprintf(outputfp, "\n");
+	pthread_mutex_unlock(&file_lock);
+
+	/* Cleanup Allocated Memory */
+	free(headptr);
+	free(hostname);
+
+	pthread_mutex_lock(&buf_lock);
+    }
+    pthread_mutex_unlock(&buf_lock);
+
+    return NULL;
 }
--- CSCI-3753/PA3/multi-lookup.h
+++ CSCI-3753/PA3/multi-lookup.h
@@ -2,8 +2,23 @@
 #define MULTILOOKUP_H
 
 #define MINARGS 3
-#define USAGE "<inputFilePath> <outputFilePath>"
+#define USAGE "<inputFilePath> ... <inputFilePath> <outputFilePath>"
 #define SBUFSIZE 1025
 #define INPUTFS "%1024s"
+
+#define BUF_SIZE 10
+
+#define MIN_RESOLVER_THREADS 2 //Required lower limit
+
+/*
+#define MAX_INPUT_FILES 10
+#define MAX_RESOLVER_THREADS 10
+#define MAX_NAME_LENGTH 1025
+#define MAX_IP_LENGTH INET6_ADDRSTRLEN
+*/
+
+void* requester(void* input);
+
+void* resolver(void* output);
 
 #endif
